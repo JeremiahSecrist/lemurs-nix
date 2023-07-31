62dcf9f76c23cf839bd55fa8a42f0fd882ee94aa
Jeremiah Secrist <jeremiah@secrist.xyz>
2023-07-30 23:49:00 -0400

    init commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs:
+    with inputs;
+      utils.lib.eachDefaultSystem (
+        system: let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in rec
+        {
+          packages = rec {
+            default = lemurs;
+            lemurs = pkgs.callPackage ./default.nix {};
+          };
+        }
+      )
+      // {
+        nixosModules = rec {
+          default = lemurs;
+          lemurs = import ./lemurs-modules.nix {};
+        };
+      };
+}
